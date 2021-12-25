b24d50e5b8d46453e91309991b6cdf96df48d441
alchemistbg <alchemist_bg@abv.bg>
2021-12-25 15:03:43 +0100

    Update scripts file
    Add working version of the javascript code

---
+++ scripts.js
@@ -0,0 +1,93 @@
+const cards = document.querySelectorAll(".card");
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+let moves = 0;
+
+function flipCard() {
+    if (lockBoard) {
+        return
+    };
+
+    if (this === firstCard) {
+        return
+    };
+
+    this.classList.add("flipped");
+    
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    }
+    
+    hasFlippedCard = false;
+    secondCard = this;
+    
+    checkForMatch();   
+}
+
+function checkForMatch() {
+    moves += 1;
+    document.querySelector(".moves-counts").innerText = moves
+    const isMatched = firstCard.dataset.cardname === secondCard.dataset.cardname;
+
+    isMatched ? disableCards() : unFlipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+    lockBoard = true; //fix another bug
+    
+    setTimeout(() => {
+        firstCard.classList.add("hidden");
+        secondCard.classList.add("hidden");
+        
+        reloadGame();
+        
+        resetBoard();
+    }, 1000);
+    
+}
+
+function unFlipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null]
+}
+
+function reloadGame() {
+    let invisibleCards = document.querySelectorAll(".hidden");
+    if (invisibleCards.length === cards.length) {
+        let resetBtn = document.querySelector(".reset");
+        resetBtn.classList.add("visible");
+
+        resetBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            document.location.reload();
+        })
+    }
+}
+
+cards.forEach((card) => {
+    card.addEventListener("click", flipCard);
+
+    let randomOrder = Math.floor(Math.random() * cards.length);
+    card.style.order = randomOrder;
+});
